d7728a5eb4acc39bbf37d663e2337d7996dcc48f
unknown <lm_dabouz@esi.dz>
2024-08-11 01:09:47 +0100

    application apres merge

--- src/pages/Admin/Publication.js
+++ src/pages/Admin/Publication.js
@@ -19,7 +19,7 @@ function PublicationPage() {
     useEffect(() => {
         getUser();
         // Initial fetch only on mount
-    }, []);
+    }, [showPopup]);
 
     const getUser = async () => {
         const token = localStorage.getItem('token');
